8261892c7cf2bca11431a9be885ce12d17e458bc
Alex Kornfeld <akorn3000@gmail.com>
2018-10-10 23:39:51 -0400

    Get things to work using fetch, add node_modules to gitignore

--- csv_to_json.js
+++ csv_to_json.js
@@ -1,36 +1,46 @@
-const csvToJSON = () => {
-  // Read in .csv file, capturing column names and individual customers
-  const fs = require("fs");
-  const text = fs.readFileSync("./data.csv").toString('utf-8');
-  const textByLine = text.split(/[\r\n]+/);
 
-  let columnNames = textByLine.slice(0, 1)[0].split(',');
-  let data = textByLine.slice(1).map(line => {
-    return line.split(',');
-  });
+// Read in .csv file, capturing column names and individual customers
+const fs = require("fs");
+const text = fs.readFileSync("./data.csv").toString('utf-8');
+const textByLine = text.split(/[\r\n]+/);
 
-  // Convert single user to JSON
-  const jsonifyUser = (user, names = columnNames) => {
-    let jsonObj = {};
-    for (let i = 0; i < user.length; i++) {
-      jsonObj[names[i]] = user[i];
-    }
+let columnNames = textByLine.slice(0, 1)[0].split(',');
+let data = textByLine.slice(1).map(line => {
+  return line.split(',');
+});
 
-    return jsonObj;
-  };
+// Convert single user to JSON
+const jsonifyUser = (user, names = columnNames) => {
+  let jsonObj = {};
+  for (let i = 0; i < user.length; i++) {
+    jsonObj[names[i]] = user[i];
+  }
 
-  // Convert all users in users array to JSON
-  const jsonifyEverybody = (usersArray, cb) => {
-    let result = [];
-
-    usersArray.forEach(user => {
-      result.push(cb(user));
-    });
-
-    return result;
-  };
-
-  return jsonifyEverybody(data, jsonifyUser);
+  return jsonObj;
 };
 
-module.exports = csvToJSON;
+// Convert all users in users array to JSON
+const jsonifyEverybody = (usersArray, cb) => {
+  let result = [];
+
+  usersArray.forEach(user => {
+    result.push(cb(user));
+  });
+
+  return result;
+};
+
+// Create JSON object of all the users
+let JSONUsers = jsonifyEverybody(data, jsonifyUser);
+
+// Write to file with JSON data
+// fs.writeFile("./data.json", JSON.stringify(JSONUsers, null, 3), (err) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//
+//   console.log("File has been created");
+// });
+
+module.exports = JSONUsers;
--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const fs = require("fs");
-const API_TOKEN = fs.readFileSync("./api_token.txt");
-const usersInJSON = require("./csv_to_json.js");
+// Necessary imports
+const fetch = require("isomorphic-fetch");
+const API_TOKEN = require("./api_token.js");
+const users = require("./csv_to_json");
 
-let headers = {
+const url = "https://api.kustomerapp.com/v1/customers";
+const headers = {
   "Content-Type": "application/json",
   "Authorization": `Bearer ${API_TOKEN}`
 };
 
-console.log(usersInJSON());
+users.forEach(userData => {
+  fetch(url, {
+    method: "POST",
+    headers,
+    data: userData
+  }).then(response => {
+    console.log(`The entity was created successfully with a ${response.status} response` );
+  }).catch(error => {
+    console.log(error);
+  });
+});
